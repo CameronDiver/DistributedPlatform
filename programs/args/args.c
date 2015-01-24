5747479f3cab2b1fefb4954c55b916b2cbc54fec
Daniel White <DanielWhite94@server.fake>
2015-01-24 19:27:20 +0000

    Args program now prints PID too.

--- programs/args/args.c
+++ programs/args/args.c
@@ -1,7 +1,9 @@
 #include "../../stdlib/stdio.h"
 #include "../../stdlib/stdlib.h"
+#include "../../stdlib/unistd.h"
 
 int main(unsigned int argc, char **argv) {
+	Dprintf("PID=%u\n", (unsigned int)Dgetpid());
 	Dprintf("argc=%u:\n", argc);
 	unsigned int i;
 	for(i=0;i<argc;++i)
