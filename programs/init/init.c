315b352aa164752c42e47af0dc318a0d0eb91dbd
Daniel White <DanielWhite94@server.fake>
2015-01-24 19:27:10 +0000

    Init program now checks it is actually the init process (i.e. getpid()==1).

--- programs/init/init.c
+++ programs/init/init.c
@@ -1,7 +1,13 @@
 #include "../../stdlib/stdio.h"
 #include "../../stdlib/stdlib.h"
+#include "../../stdlib/unistd.h"
 
 int main(unsigned int argc, char **argv) {
+	// Check we are the init process.
+	pid_t pid=Dgetpid();
+	if (pid!=1)
+		return EXIT_FAILURE;
+	
 	Dprintf("Hello world from init!\n");
 	return EXIT_SUCCESS;
 }
