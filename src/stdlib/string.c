0aee2725ecc558304c929ce2564e164e08113c0c
Daniel White <DanielWhite94@server.fake>
2015-01-30 18:56:33 +0000

    Added memcpy and memmove to stdlib.

--- src/stdlib/string.c
+++ src/stdlib/string.c
@@ -12,3 +12,15 @@ size_t Dstrlen(const char *s)	{
 	for(len=0;*s!='\0';++len) ++s;
 	return len;
 }
+
+void *Dmemcpy(void *dest, const void *src, size_t n) {
+	return Dmemmove(dest, src, n);
+}
+
+void *Dmemmove(void *dest, const void *src, size_t n) {
+	char *d=(char *)dest;
+	const char *s=(const char *)s;
+	while(n-->0)
+		*d++=*s++;
+	return dest;
+}
--- src/stdlib/string.h
+++ src/stdlib/string.h
@@ -7,4 +7,8 @@ void *Dmemset(void *s, int c, size_t n);
 
 size_t Dstrlen(const char *s);
 
+
+void *Dmemcpy(void *dest, const void *src, size_t n);
+void *Dmemmove(void *dest, const void *src, size_t n);
+
 #endif
