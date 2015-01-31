ccf7395e3a6365850bb1b87846249941250d4f99
Daniel White <DanielWhite94@server.fake>
2015-01-31 19:04:21 +0000

    Added various functions to string.h: strchr, strspan, strcspan, strtok and strtok_r.

--- src/stdlib/string.c
+++ src/stdlib/string.c
@@ -1,5 +1,7 @@
 #include "string.h"
 
+char *strtokSave;
+
 void *Dmemset(void *s, int c, size_t n) {
 	unsigned char *d=s;
 	while (n-->0)
@@ -23,4 +25,55 @@ void *Dmemmove(void *dest, const void *src, size_t n) {
 	while(n-->0)
 		*d++=*s++;
 	return dest;
+}
+
+char *Dstrchr(const char *s, int c) {
+	for(;*s!=c;++s)
+		if (*s=='\0')
+			return NULL;
+	return (char *)s;
+}
+
+size_t Dstrspn(const char *s, const char *accept) {
+  size_t n;
+  const char* p;
+  for(n=0; *s; s++, n++) {
+    for(p=accept; *p && *p != *s; p++)
+      ;
+    if (!*p)
+      break;
+  }
+  return n;
+}
+
+size_t Dstrcspn(const char *s, const char *reject) {
+	size_t count;
+	for(count=0;Dstrchr(reject, *s)==0;++count)
+		++s;
+	return count;
+}
+
+char *Dstrtok(char *str, const char *delim) {
+	return Dstrtok_r(str, delim, &strtokSave);
+}
+
+char *Dstrtok_r(char *str, const char *delim, char **saveptr) {
+	char *ret;
+	
+	// Continuing previous search?
+	if (str==NULL)
+		str=*saveptr;
+	
+	// Find next delim.
+	str+=Dstrspn(str, delim);
+	if (*str=='\0')
+		return NULL;
+	ret=str;
+	
+	str+=Dstrcspn(str, delim);
+	if(*str)
+		*str++='\0';
+	*saveptr=str;
+	
+	return ret;
 }
--- src/stdlib/string.h
+++ src/stdlib/string.h
@@ -7,8 +7,15 @@ void *Dmemset(void *s, int c, size_t n);
 
 size_t Dstrlen(const char *s);
 
-
 void *Dmemcpy(void *dest, const void *src, size_t n);
 void *Dmemmove(void *dest, const void *src, size_t n);
+
+char *Dstrchr(const char *s, int c);
+
+size_t Dstrspn(const char *s, const char *accept);
+size_t Dstrcspn(const char *s, const char *reject);
+
+char *Dstrtok(char *str, const char *delim);
+char *Dstrtok_r(char *str, const char *delim, char **saveptr);
 
 #endif
