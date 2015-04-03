bb6b1691570e581269bdd12c7e357530655a910b
Daniel White <danielwhite94@fake.com>
2015-04-03 11:21:05 +0100

    Formatting changes.

--- src/stdlib/syscall.c
+++ src/stdlib/syscall.c
@@ -4,39 +4,33 @@
 void (*sys_syscall)(void *, uint32_t, ...)=NULL;
 void *sys_data=NULL;
 
-void sys_init(void (*syscall)(void *, uint32_t id, ...), void *syscallData)
-{
+void sys_init(void (*syscall)(void *, uint32_t id, ...), void *syscallData) {
 	sys_syscall=syscall;
 	sys_data=syscallData;
 }
 
-void sys_exit(uint32_t status)
-{
+void sys_exit(uint32_t status) {
 	(*sys_syscall)(sys_data, 0, status);
 }
 
-uint32_t sys_fork(void)
-{
+uint32_t sys_fork(void) {
 	uint32_t ret;
 	(*sys_syscall)(sys_data, 1, &ret);
 	return ret;
 }
 
-uint32_t sys_getpid(void)
-{
+uint32_t sys_getpid(void) {
 	uint32_t ret;
 	(*sys_syscall)(sys_data, 2, &ret);
 	return ret;
 }
 
-void *sys_alloc(void *ptr, size_t size)
-{
+void *sys_alloc(void *ptr, size_t size) {
 	void *ret;
 	(*sys_syscall)(sys_data, 3, &ret, ptr, size);
 	return ret;
 }
 
-void sys_exec(const char *path, uint32_t argc, char **argv)
-{
+void sys_exec(const char *path, uint32_t argc, char **argv) {
 	(*sys_syscall)(sys_data, 4, path, argc, argv);
 }
