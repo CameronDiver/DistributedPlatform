08699c80d47abfb9653145e2ae4794f68d89ab40
Daniel White <DanielWhite94@server.fake>
2015-04-19 23:12:17 +0100

    Tidied up setup.sh slightly.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+# Make directory structure.
 mkdir -p ./container
 cd ./container
-mkdir -p ./bin
-mkdir -p ./sys
+	mkdir -p ./bin
+	mkdir -p ./sys
 cd ../
 
+# Copy programs.
 cd ./src/programs
 	cd ./init/src
 	make clean
@@ -19,35 +22,35 @@ cd ./src/programs
 	cd ../bin
 	cp ./factor ../../../../container/bin
 	cd ../../
-	
+
 	cd ./true/src
 	make clean
 	make
 	cd ../bin
 	cp ./true ../../../../container/bin
 	cd ../../
-	
+
 	cd ./false/src
 	make clean
 	make
 	cd ../bin
 	cp ./false ../../../../container/bin
 	cd ../../
-	
+
 	cd ./yes/src
 	make clean
 	make
 	cd ../bin
 	cp ./yes ../../../../container/bin
 	cd ../../
-	
+
 	cd ./rabbits/src
 	make clean
 	make
 	cd ../bin
 	cp ./rabbits ../../../../container/bin
 	cd ../../
-	
+
 	cd ./args/src
 	make clean
 	make
@@ -63,6 +66,7 @@ cd ./src/programs
 	cd ../../
 cd ../../
 
+# Build server and create boot executable.
 cd ./src/server
 make clean
 make
