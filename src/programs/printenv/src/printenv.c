4eb2b18ecca0e889c15fa64c8a3b23cfc43c529d
Daniel White <DanielWhite94@server.fake>
2015-04-21 21:06:17 +0100

    printenv program now uses Denviron not environ. Also fixed makefile.

--- src/programs/printenv/src/printenv.c
+++ src/programs/printenv/src/printenv.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern char **environ;
+extern char **Denviron;
 
 int main(int argc, char **argv) {
 	char **ptr;
-	for(ptr=environ;*ptr!=NULL;++ptr)
-		puts(*ptr);
+	for(ptr=Denviron;*ptr!=NULL;++ptr)
+		Dprintf("%s\n", *ptr);
 	return EXIT_SUCCESS;
 }
